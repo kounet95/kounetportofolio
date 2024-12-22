c014b80a67eb78a5f41209b5ea111eef1afb14c6
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-12-22 00:49:43 -0500

    v1

---
+++ kounettechnologieportofolio/src/test/java/com/example/kounettechnologieportofolio/KounettechnologieportofolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kounettechnologieportofolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KounettechnologieportofolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
